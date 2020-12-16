9d349d1791a5c0193ed7c7d68a00bb8a42a38bbf
Faustas Butkus <faustas.butkus@gmail.com>
2020-12-16 11:45:33 +0200

    always return zedge logo

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ def put_mustaches():
     image_url = request.args.get('url')
     print(image_url)
 
-    with ImageProvider(image_url, 'hello') as image_path:
-        return prepare_image_base64('images/zedge.png')
+    # with ImageProvider(image_url, 'hello') as image_path:
+    return prepare_image_base64('images/zedge.png')
 
 
 if __name__ == '__main__':
